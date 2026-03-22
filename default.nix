1390769470f94ec53a1b527f0989a34b8df86357
Carlos Junior <cjuniorfox@gmail.com>
2026-03-21 21:42:50 -0300

    add Nix package definition and installation instructions

---
+++ default.nix
@@ -0,0 +1,36 @@
+{ lib, stdenvNoCC, python3, librsvg, xorg }:
+
+stdenvNoCC.mkDerivation rec {
+  pname = "bibata-cursor";
+  version = "0.2.0";
+
+  src = ./.;
+
+  nativeBuildInputs = [
+    python3
+    librsvg
+    xorg.xcursorgen
+  ];
+
+  dontConfigure = true;
+
+  buildPhase = ''
+    runHook preBuild
+    python3 cursor_utils.py --hypr --x11 --out-dir out
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p "$out/share/icons"
+    cp -r out/Bibata-* "$out/share/icons/"
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Bibata cursor theme for X11 and Hyprland";
+    homepage = "https://github.com/rtgiskard/bibata_cursor";
+    license = licenses.gpl3Plus;
+    platforms = platforms.linux;
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Bibata cursor theme for X11 and Hyprland";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in {
+          default = pkgs.callPackage ./default.nix {};
+        });
+    };
+}
